87925989171dc65ca18efe05a63b882bb46e2c75
Abu_Da7em <you@example.com>
2016-04-30 22:41:34 +0300

    ssdf

---
+++ src/ng2-commons/layout/icon.directive.ts
@@ -0,0 +1,28 @@
+import {Component,Input,Optional} from 'angular2/core';
+@Component({
+  selector:'sc-icon',
+  host:{
+    'style':';display:inline-block;white-space:nowrap',
+    '(mouseenter)':'showLabel()',
+    '(mouseleave)':'hideLabel()'
+  },
+  styles:['.rounded{border-radius:50%}'],
+  template:`<i class="material-icons {{cssClasses}}" [style.backgroundColor]="backgroundColor"
+              [style.fontSize]="fontSize" [style.color]="fontColor ? fontColor:'inherit'">
+              {{name}}
+            </i>
+            <span *ngIf="labelShown && label">{{label}}</span>`
+})
+export class IconDirective{
+  @Input() name:string;
+  @Input() cssClasses:string;
+  @Input('font-size') fontSize:string;
+  @Input() backgroundColor:string;
+  @Input() fontColor:string;
+  @Input() label:string;
+  @Optional() dir = 'ltr';
+  labelShown:boolean=false;
+
+  showLabel(){ this.labelShown = this.label ? true:false }
+  hideLabel(){ this.labelShown = false}
+}
