4f0b3255395753dc5e0e4322b21f1268ef7fbe49
Abu_Da7em <you@example.com>
2016-05-10 06:11:45 +0300

    gulp commit

--- layout/flex.ts
+++ layout/flex.ts
@@ -4,11 +4,15 @@ import {Directive,Input,ElementRef,Renderer} from '@angular/core';
   selector:'[flex]'
 })
 export class FlexDirective{
-  @Input() flex;
-  constructor(private renderer:Renderer, private el:ElementRef){
-  }
-  ngOnChanges(){
-    var flexString = this.flex === ''? '1':'0 0 ' + this.flex +'%';
-    this.renderer.setElementStyle(this.el.nativeElement,'flex',flexString);
-  }
+    @Input() shrink:number = 1;
+    @Input() grow:number = 1;
+    @Input() basis:string = '0%';
+    
+    constructor(private renderer:Renderer, private el:ElementRef){}
+    
+    @Input()
+    set flex(f){
+        let flexString = (f === '') ? '1 1 0%' : `${this.grow} ${this.shrink} ${f}%`;
+        this.renderer.setElementStyle(this.el.nativeElement,'flex',flexString);
+    }
 }
