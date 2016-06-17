23984f33d3efd6d6f0183f34e35b4ae50af4cbe1
Abu_Da7em <you@example.com>
2016-06-17 05:11:51 +0300

    ssdf

---
+++ layout/index.ts
@@ -0,0 +1,4 @@
+export {FlexDirective} from './flex';
+export {IconDirective} from './icon.directive';
+export {LayoutDirective} from './layout';
+export {ResponsiveService,ResponsivePipe} from './responsive.service';
